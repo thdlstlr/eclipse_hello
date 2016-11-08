ca32f445e4d15d7d73eb89c0a1d8b0158e3a693c
thdlstlr <thdlstlr@nate.com>
2016-11-08 14:39:30 +0900

    PushPrint.java modified on GitHub

--- HelloWorld/src/PushPrint.java
+++ HelloWorld/src/PushPrint.java
@@ -4,7 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java File Push");
-
+		Sytem.out.println("World with 365");
 	}
 
 }
